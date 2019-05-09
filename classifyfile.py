6049e5bab3a15109ab98a600173c66717b52ac9f
supmatch <120612671@qq.com>
2019-05-09 10:37:25 +0800

    the first commit

---
+++ classifyfile.py
@@ -0,0 +1,26 @@
+import os
+
+def eachFile(filepath):
+    files_path = os.listdir(filepath)
+    sql_type =['sql']
+    xml_type = ['xml']
+    for fi in files_path:
+        fi_d = os.path.join(filepath, fi)
+        if os.path.isdir(fi_d):
+            eachFile(fi_d)
+        else:
+            child = os.path.join(fi_d)
+            arrs = child.split('.')[-1]
+            if arrs in sql_type:
+                sql_files.append(child)
+            if arrs in xml_type:
+                xml_files.append(child)
+    return sql_files, xml_files
+
+
+if __name__ == "__main__":
+	search_dir = search_dir
+	sql_files, xml_files=[], []
+	sql_files, xml_files = eachFile(base_dir)
+	print(sql_files)
+	print(xml_files)
